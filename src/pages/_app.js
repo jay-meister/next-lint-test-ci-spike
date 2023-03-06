c7c1578e139e0adbad71ddd2fdb6e240a7803e35
Jack Murphy <jmurphy.web@gmail.com>
2023-03-06 13:09:27 +0000

    run npm run lint:fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+
+import PropTypes from "prop-types";
+
+App.propTypes = {
+  Component: PropTypes.component.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
